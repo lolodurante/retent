9e8c6ce2d83449fccdd598804e410f512eff907c
Lorenzo Durante <lolodurante2003@gmail.com>
2024-02-04 13:16:12 -0300

    Removed unnecesary sidebar items

--- src/routes.tsx
+++ src/routes.tsx
@@ -17,18 +17,18 @@ const routes = [
     layout: '/admin',
     path: 'default',
     icon: <MdHome className="h-6 w-6" />,
-  },
-  {
-    name: 'Profile',
-    layout: '/admin',
-    path: 'profile',
-    icon: <MdPerson className="h-6 w-6" />,
-  },
-  {
-    name: 'Sign In',
-    layout: '/auth',
-    path: 'sign-in',
-    icon: <MdLock className="h-6 w-6" />,
-  },
+  }
+//  {
+    //name: 'Profile',
+    //layout: '/admin',
+    //path: 'profile',
+    //icon: <MdPerson className="h-6 w-6" />,
+  //},
+  //{
+    //name: 'Sign In',
+    //layout: '/auth',
+    //path: 'sign-in',
+    //icon: <MdLock className="h-6 w-6" />,
+  //},
 ];
 export default routes;
